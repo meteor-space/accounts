43766392ba21af688e6aa084d5bb723e80607176
Dominik Guzei <dominik.guzei@gmail.com>
2015-12-02 12:14:46 +0100

    fixes problem with multi-registrations of EJSON type

--- source/server/commands.js
+++ source/server/commands.js
@@ -1,12 +1,18 @@
 
 Space.accounts.configureCommands = function(configuration) {
 
-  Space.messaging.define(Space.messaging.Command, 'Space.accounts', {
+  let commandWasNotDefinedYet = Space.accounts.SignupUser === undefined;
 
-    SignupUser: _.extend(configuration.accounts.signupUserCommand, {
+  // Define or override existing signup command
+  Space.messaging.Command.extend(Space.accounts, 'SignupUser',
+    _.extend(configuration.accounts.signupUserCommand, {
       targetId: Guid // Meteor.user GUID
     })
+  );
 
-  });
+  // Only register EJSON type once
+  if (commandWasNotDefinedYet) {
+    Space.accounts.SignupUser.type('Space.accounts.SignupUser');
+  }
 
 };
