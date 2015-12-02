7d8b1247b8ff10d8dad7c400bf5bfe95a9bf709f
Dominik Guzei <dominik.guzei@gmail.com>
2015-12-02 12:38:52 +0100

    fixes signup command fields

--- source/server/commands.js
+++ source/server/commands.js
@@ -4,11 +4,13 @@ Space.accounts.configureCommands = function(configuration) {
   let commandWasNotDefinedYet = Space.accounts.SignupUser === undefined;
 
   // Define or override existing signup command
-  Space.messaging.Command.extend(Space.accounts, 'SignupUser',
-    _.extend(configuration.accounts.signupUserCommand, {
-      targetId: Guid // Meteor.user GUID
-    })
-  );
+  Space.messaging.Command.extend(Space.accounts, 'SignupUser', {
+    statics: {
+      fields: _.extend(configuration.accounts.signupUserCommand, {
+        targetId: Guid // Meteor.user GUID
+      })
+    }
+  });
 
   // Only register EJSON type once
   if (commandWasNotDefinedYet) {
